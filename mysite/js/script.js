f4b67d196faa6845d81a1680b6dcd25a2159838e
龘龘丶 <41265224+RZFan525@users.noreply.github.com>
2019-11-03 20:54:49 +0800

    Add files via upload

---
+++ mysite/js/script.js
@@ -0,0 +1,58 @@
+$(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['darkcyan', '#2AB561', '#826B68', '#EA6153', "#993366"],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        navigation: true,
+        navigationPosition: "right",
+        navigationTooltips: ['个人信息', '专业技能', '学科竞赛', '科研经历', '获奖情况'],
+        menu: "#menu",
+        afterLoad: function(afterlink, index) {
+            var $anim = $(".section").eq(index.index).find(".animated");
+
+            function addAnimated(flag) {
+                if (flag) {
+                    $anim.each(function(i) {
+                        var that = this;
+                        setTimeout(function() {
+                            $(that).addClass($(that).data('anim')).css({ visibility: 'visible' })
+                        }, 100 * i)
+                    });
+                } else {
+                    $anim.each(function() {
+                        $(this).addClass($(this).data('anim')).css({ visibility: 'visible' })
+                    });
+                }
+            }
+            switch (index.index) {
+                case 1:
+                    addAnimated();
+                    break;
+                case 2:
+                    addAnimated(1);
+                    break;
+                case 3:
+                    $('.line').animate({ "height": "100%" }, 1000);
+                    addAnimated();
+                    break;
+                case 4:
+                    addAnimated(1);
+                    break;
+                case 5:
+                    setTimeout(function() {
+                        $('#fifth .overT,#fifth .overB').animate({ 'height': '30%' }, 400).find('#callme').addClass("callme");
+                    }, 400);
+                    break;
+
+            }
+        },
+        onLeave: function(index, nextIndex, direction) {
+            var $anim = $(".section").eq(index.index).find(".animated");
+            $anim.each(function(i) {
+                var that = this;
+                setTimeout(function() {
+                    $(that).removeClass($(that).data('anim')).css({ visibility: 'hidden' });
+                }, 100 * i)
+            });
+        }
+    });
+});
